c83ebf5830cf7794ad74252367f7e5f018249ccd
Ankit Prajapati <77499177+ankit84-tech@users.noreply.github.com>
2024-01-13 00:29:22 +0530

    Added app bar in home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -25,6 +25,9 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+      title: const Text('Habit Tracker'),
+    ),
         backgroundColor: Colors.grey[300],
         body: ListView.builder(
             itemCount: todayHabitList.length,
